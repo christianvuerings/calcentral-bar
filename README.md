56087edab326c013412e985b3b0f7e902a0d6534
Christian Vuerings <vueringschristian@gmail.com>
2014-11-01 07:21:16 -0700

    Update JS and install directorions

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ To build the extension use:
 chmod +x build.sh
 ./build.sh
 ```
+
+## Install locally into PeopleSoft
+
+1. Change the `doctype`.
+You should change the doctype to `<!doctype html>` in `PT_HNAV_TEMPLATE`.
+
+1. Remove the `IE8` compat tag.
+Remove `<meta http-equiv="X-UA-Compatible" content="IE=8">` in `PT_HNAV_TEMPLATE`.
+
+1. Insert the JavaScript
+Add the JavaScript on the bottom of `PT_PORTAL_AS_JS`.
+
+1. Add the CSS
+Create a CSS Object in the branding page and add it to `PSSTYLEDEF` as a sub-stylesheet.
--- src/js/calcentral-bar.js
+++ src/js/calcentral-bar.js
@@ -14,7 +14,9 @@
     calcentralLogo.setAttribute('href', homeLink.href);
     calcentralLogo.setAttribute('title', 'Return Home');
     calcentralLogo.setAttribute('class', 'cc-calcentral-logo');
-    calcentralLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="45" height="35" viewBox="227.8 181.7 45 35" enable-background="new 227.8 181.7 45 35"><path fill="#FDB515" d="M228.2 216.7c.1-.3.3-.7.5-.9.4-.5 1.1-1.1 2-1.7 1.4-.9 2.7-1.5 4.3-1.9 2.4-.6 4.9-.7 7.1-.7h.1c2.9 0 9.1.4 13.6.6 2.4.1 4.3.3 4.9.3h1c2.8 0 3.7-.4 4.1-.7.5-.3 1-.6.9-.9-.1-.4-1-.4-1.4-.4-1.4-.2-2.7-.8-3.6-1.4-1-.7-1.7-2-1.7-2l-.7.5c-2.5 1.7-4.5 2.4-6.5 2.4h-.3c-1.1 0-2-.5-2.2-.9l-.4-.7-.7.6c-.9.8-2.1 1-2.6 1.1h-.6c-1.1 0-2.1-.4-2.9-1.1-.1-.1-.5-.6-.6-.7l-.5-.7-.6.5c-.2.2-.9.6-1 .7-1.4.8-3 1.2-4.5 1.2-1.4 0-2.7-.3-3.9-1-2.7-1.5-3.1-4.5-3-6.8.2-3.1 1.3-6.1 3.3-9.1 1.7-2.6 4.2-5 7-7.1 2-1.5 5.2-3.4 8.5-3.9.6-.1 1.2-.2 1.8-.2.7 0 1.2.1 1.6.3 1 .6 1 1.9.9 2.5-.3 2.5-2.6 5.5-5 5.5-.5 0-.9-.2-1.1-.5-.2-.2-.2-.5-.1-.8.2-.8 1-1.1 2.2-1.4.1 0 .2-.1.3-.1 2-.6 2.3-1.8 2.2-2.7-.1-.6-.5-1.3-1.6-1.3-1 0-2.2.6-2.4.7-2.7 1.4-4.5 3.6-6.3 5.9-3.6 4.9-5.4 9.5-5.1 13.9.1 1.9 1.4 4 3.4 4 .7 0 1.3-.2 1.9-.7 1-.7 1.6-1.9 2.1-2.9.1-.3.3-.5.4-.8.8-1.4 1.7-2.6 2.7-3.7 1.2-1.1 2.5-1.9 3.9-2.3.7-.2 1.4-.3 2.2-.3.8 0 1.2.5 1.4.8.1-.3.4-.7.4-.8.1-.1 1.4-.1 1.8 0 .3 0 1.4 0 1.5.1.2.2-.3 1-.7 2.3-.2.6-.5 1.3-.9 2.2-.1.3-.3.7-.4 1-1.1 2.7-1.8 4.5-.6 5.1.2.1.5.2.8.2.8 0 1.7-.6 2.3-1 .1 0 .1-.1.2-.1.7-.5 1.1-.8 1.5-1.1l.2-.2v-.3c-.2-3 .2-6.3 1.1-8.5 1.3-3.2 3.1-6 5.3-8.2 1.1-1.1 3.9-3.2 5.5-3.2 1.3 0 .9 2.3.8 2.8-.3 1.2-1.6 4.8-3.9 8.6-.8 1.3-2.4 3.5-3.5 5.1-.5.7-.9 1.3-1.1 1.5l-.1.2v.2c-.2 1.8.8 3.6 1.8 4.6.9 1 1.9 1.3 2.8 1.6.3.1.7.2 1 .4.3.1.7.3.7.6.1.3-.1.7-.5 1.2-2 2.5-4.5 3.2-6.9 3.7-1.4.3-3.1.4-5.2.4-1.4 0-2.8-.1-4.2-.1-.5 0-1 0-1.5-.1-1.6-.1-3.3-.2-4.9-.2-1.5-.1-3-.2-4.3-.2-1.1 0-2.2-.1-3.1-.1-2.7 0-5 .2-6.9.5-.9.1-1.6.3-2.3.5-.2.1-.4.1-.6.2-.1 0-.2.1-.3.1-.1-.3-.5-.2-.8-.2zm23.7-18.3c-.7 0-2.5.9-3.9 3.6-.3.6-1.4 3.2-.8 4.4.2.4.5.6.9.7h.3c1.2 0 2.1-1.7 3-3.7.1-.2.1-.3.2-.4.2-.5 1.1-2.6 1.3-3.4 0-.5.1-1.2-1-1.2zm18.9-10.6c-.3-.2-1.1.4-1.1.4-1.5 1.2-2.6 2.7-3.6 4.4-2.6 4.5-2.3 8-2.3 8s1.3-1.5 1.6-2c.4-.7 2-2.9 3-4.5 1-1.7 2.1-4.1 2.4-4.9 0-.1.4-1.1 0-1.4z"/></svg>';
+    calcentralLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="45" height="35" viewBox="227.8 181.7 45 35" enable-background="new 227.8 181.7 45 35">' +
+      '<path fill="#fdb515" d="M228.2 216.7c.1-.3.3-.7.5-.9.4-.5 1.1-1.1 2-1.7 1.4-.9 2.7-1.5 4.3-1.9 2.4-.6 4.9-.7 7.1-.7h.1c2.9 0 9.1.4 13.6.6 2.4.1 4.3.3 4.9.3h1c2.8 0 3.7-.4 4.1-.7.5-.3 1-.6.9-.9-.1-.4-1-.4-1.4-.4-1.4-.2-2.7-.8-3.6-1.4-1-.7-1.7-2-1.7-2l-.7.5c-2.5 1.7-4.5 2.4-6.5 2.4h-.3c-1.1 0-2-.5-2.2-.9l-.4-.7-.7.6c-.9.8-2.1 1-2.6 1.1h-.6c-1.1 0-2.1-.4-2.9-1.1-.1-.1-.5-.6-.6-.7l-.5-.7-.6.5c-.2.2-.9.6-1 .7-1.4.8-3 1.2-4.5 1.2-1.4 0-2.7-.3-3.9-1-2.7-1.5-3.1-4.5-3-6.8.2-3.1 1.3-6.1 3.3-9.1 1.7-2.6 4.2-5 7-7.1 2-1.5 5.2-3.4 8.5-3.9.6-.1 1.2-.2 1.8-.2.7 0 1.2.1 1.6.3 1 .6 1 1.9.9 2.5-.3 2.5-2.6 5.5-5 5.5-.5 0-.9-.2-1.1-.5-.2-.2-.2-.5-.1-.8.2-.8 1-1.1 2.2-1.4.1 0 .2-.1.3-.1 2-.6 2.3-1.8 2.2-2.7-.1-.6-.5-1.3-1.6-1.3-1 0-2.2.6-2.4.7-2.7 1.4-4.5 3.6-6.3 5.9-3.6 4.9-5.4 9.5-5.1 13.9.1 1.9 1.4 4 3.4 4 .7 0 1.3-.2 1.9-.7 1-.7 1.6-1.9 2.1-2.9.1-.3.3-.5.4-.8.8-1.4 1.7-2.6 2.7-3.7 1.2-1.1 2.5-1.9 3.9-2.3.7-.2 1.4-.3 2.2-.3.8 0 1.2.5 1.4.8.1-.3.4-.7.4-.8.1-.1 1.4-.1 1.8 0 .3 0 1.4 0 1.5.1.2.2-.3 1-.7 2.3-.2.6-.5 1.3-.9 2.2-.1.3-.3.7-.4 1-1.1 2.7-1.8 4.5-.6 5.1.2.1.5.2.8.2.8 0 1.7-.6 2.3-1 .1 0 .1-.1.2-.1.7-.5 1.1-.8 1.5-1.1l.2-.2v-.3c-.2-3 .2-6.3 1.1-8.5 1.3-3.2 3.1-6 5.3-8.2 1.1-1.1 3.9-3.2 5.5-3.2 1.3 0 .9 2.3.8 2.8-.3 1.2-1.6 4.8-3.9 8.6-.8 1.3-2.4 3.5-3.5 5.1-.5.7-.9 1.3-1.1 1.5l-.1.2v.2c-.2 1.8.8 3.6 1.8 4.6.9 1 1.9 1.3 2.8 1.6.3.1.7.2 1 .4.3.1.7.3.7.6.1.3-.1.7-.5 1.2-2 2.5-4.5 3.2-6.9 3.7-1.4.3-3.1.4-5.2.4-1.4 0-2.8-.1-4.2-.1-.5 0-1 0-1.5-.1-1.6-.1-3.3-.2-4.9-.2-1.5-.1-3-.2-4.3-.2-1.1 0-2.2-.1-3.1-.1-2.7 0-5 .2-6.9.5-.9.1-1.6.3-2.3.5-.2.1-.4.1-.6.2-.1 0-.2.1-.3.1-.1-.3-.5-.2-.8-.2zm23.7-18.3c-.7 0-2.5.9-3.9 3.6-.3.6-1.4 3.2-.8 4.4.2.4.5.6.9.7h.3c1.2 0 2.1-1.7 3-3.7.1-.2.1-.3.2-.4.2-.5 1.1-2.6 1.3-3.4 0-.5.1-1.2-1-1.2zm18.9-10.6c-.3-.2-1.1.4-1.1.4-1.5 1.2-2.6 2.7-3.6 4.4-2.6 4.5-2.3 8-2.3 8s1.3-1.5 1.6-2c.4-.7 2-2.9 3-4.5 1-1.7 2.1-4.1 2.4-4.9 0-.1.4-1.1 0-1.4z"/>' +
+      '</svg>';
     topContainer.appendChild(calcentralLogo);
   };
 
@@ -30,21 +32,14 @@
   };
 
   var addLinkEvent = function(element) {
-    var id = element.id;
-    var psEvent = '';
-    if (id === 'pthdr2mcf') {
-      psEvent = 'PopupMCFConsole';
-    } else if (id === 'pthdr2cti') {
-      psEvent = 'PopupCTIConsole';
-    } else if (id === 'pthdr2ppm') {
-      psEvent = 'PopupPPMConsole';
-    }
-
-    if (psEvent) {
+    var id = element.getAttribute('data-id');
+    if (id) {
       element.addEventListener('click', function(evt) {
-        if (window[psEvent]) {
-          window[psEvent]();
-          evt.preventDefault();
+        evt.preventDefault();
+        evt.stopPropagation();
+        var clickElement = document.getElementById(id);
+        if (clickElement.click) {
+          clickElement.click();
         }
       });
     }
@@ -60,9 +55,9 @@
     menuButton.appendChild(menuButtonText);
 
     //  Toggle the class on click to show / hide the menu
-    menuButton.onclick = function() {
+    menuButton.addEventListener('click', function() {
       this.classList.toggle('cc-calcentral-menu-toggle-active');
-    };
+    });
     nav.appendChild(menuButton);
 
     var ul = document.createElement('ul');
@@ -70,8 +65,16 @@
     var topLinks = document.querySelectorAll('#pthdr2links a');
     for (var i = 0; i < topLinks.length; i++) {
       var li = document.createElement('li');
-      // addLinkEvent(topLinks[i]);
-      li.appendChild(topLinks[i]);
+      var a = document.createElement('a');
+      var item = topLinks[i];
+      a.setAttribute('href', item.href);
+      if (item.id) {
+        a.setAttribute('data-id', item.id);
+      }
+      var aText = document.createTextNode(item.textContent);
+      a.appendChild(aText);
+      addLinkEvent(a);
+      li.appendChild(a);
       ul.appendChild(li);
     }
     nav.appendChild(ul);
@@ -79,8 +82,7 @@
   };
 
   var addContainer = function() {
-    console.log(topPeopleSoftContainer.parentNode);
-    topPeopleSoftContainer.parentNode.replaceChild(topContainer, topPeopleSoftContainer);
+    topPeopleSoftContainer.parentNode.appendChild(topContainer);
   };
 
   var addTopBar = function() {
@@ -91,6 +93,18 @@
     addContainer();
   };
 
-  addTopBar();
+  var findTopBar = function() {
+    topPeopleSoftContainer = document.getElementById('pthdr2container');
+    if (topPeopleSoftContainer) {
+      addTopBar();
+    } else {
+      // Sometimes this gets loaded too fast, so we need to retry in that case
+      setTimeout(findTopBar, 100);
+    }
+  };
+
+  setTimeout(function() {
+    findTopBar();
+  }, 1);
 
 })();
